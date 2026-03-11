093e90cbb6d22922646bcca532386cef9de70667
petar-romic-179-25 <petar.romic.179.25@ict.edu.rs>
2026-03-11 14:03:25 +0100

    Create main.c
    Program pisan u C-u

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Uključuje biblioteku za ulaz/izlaz
+
+int main() {
+    // printf funkcija štampa tekst na ekranu
+    printf("Petar Romic\n");                
+    return 0; // Označava uspešan završetak programa
+}
